0be0e9012b13d3e3a37436fac60681042f6b7cc6
Yakushev Timur <tyakushev@ozon.ru>
2021-12-16 14:42:01 +0300

    Initial commit

---
+++ PrimeNumbersFun.py
@@ -0,0 +1,111 @@
+from math import *
+import time
+
+
+def checkIfDividedByThree(num):
+    num1 = str(num)
+    sum = 0
+    for n in num1:
+        sum += int(n)
+    if sum%3 == 0:
+        return True
+    else:
+        return False
+
+
+def isPrimeHighestSpeed(a):
+    if a == 2:
+        return True
+    i = 3
+    b = sqrt(a)
+    if a % 2 == 0:
+        return False
+    checkIfDividedByThree(a)
+    while i <= b:
+        if a % i == 0:
+            return False
+        i += 2
+    return True
+
+def isPrimeHighSpeed(a):
+    if a == 2:
+        return True
+    i = 3
+    b = sqrt(a)
+    if a % 2 == 0:
+        return False
+    while i <= b:
+        if a % i == 0:
+            return False
+        i += 2
+    return True
+
+def isPrimeMidSpeed(a):
+    i = 2
+    b = sqrt(a)
+    while i <= b:
+        if a % i == 0:
+            return False
+        i += 1
+    return True
+
+def isPrimeLowSpeed(a):
+    i = 2
+    while i < a:
+        if a % i == 0:
+            return False
+        i += 1
+    return True
+
+def findSimpleUntilLow(until):
+    b = []
+    i = 1
+    while i < until:
+        if isPrimeLowSpeed(i):
+            b.append(i)
+        i += 1
+    return b
+
+
+def findSimpleUntilMid(until):
+    b = []
+    i = 1
+    while i < until:
+        if isPrimeMidSpeed(i):
+            b.append(i)
+        i += 1
+    return b
+
+
+def findSimpleUntil(until, mode):
+    b = []
+    i = 1
+    while i < until:
+        if mode == 1 and isPrimeLowSpeed(i):
+            b.append(i)
+        elif mode == 2 and isPrimeMidSpeed(i):
+            b.append(i)
+        elif mode == 3 and isPrimeHighSpeed(i):
+            b.append(i)
+        elif mode == 4 and isPrimeHighestSpeed(i):
+            b.append(i)
+        i += 1
+    return b
+
+start_time = time.time()
+a = 1000000
+#findSimpleUntil(a, 1)
+print("--- %s seconds Low Speed math---" % (time.time() - start_time))
+
+start_time = time.time()
+findSimpleUntil(a, 2)
+print("--- %s seconds Mid Speed math---" % (time.time() - start_time))
+
+start_time = time.time()
+findSimpleUntil(a, 3)
+print("--- %s seconds High Speed math---" % (time.time() - start_time))
+
+start_time = time.time()
+findSimpleUntil(a, 4)
+print("--- %s seconds Highest Speed math---" % (time.time() - start_time))
+
